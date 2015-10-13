188d7b94e56cb32c9ef594aa52f2299c3cad1255
Jayant Bhawal <bhawal.jayant@gmail.com>
2015-10-13 07:54:32 +0530

    Can remove local profile now.

--- app/components/profile-container.js
+++ app/components/profile-container.js
@@ -15,12 +15,27 @@ export default Ember.Component.extend({
 	},
 	userdata: {},
 	hasUser: false,
+	message: '',
 	actions:{
 		addUser(newUser){
 			this.sendAction('action', newUser);
 			this.set('hasUser',true);
 			this.loadUser();
 			//console.log(hasUser);
+			var i = 3;
+			var self = this;
+			var clr = setInterval(function () {
+				self.set('message','This page will redirect to feeds in '+i+' second(s).');
+				i--;
+				if(i === 0){
+					clearInterval(clr);
+					self.transitionTo('index');
+				}
+			},1000);
+		},
+		deleteUserData: function () {
+			Cookies.remove('userdata');
+			this.set('hasUser',false);
 		}
 	}
 });
